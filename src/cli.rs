5806889b497e52139e27f183ab2aa98677a43a6f
Matthew Franchini <matchewf11@gmail.com>
2026-05-22 11:31:32 -0700

    serde

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,29 @@
-use clap::Parser;
+use clap::{
+    Parser,
+    Subcommand,
+};
+
+// <https://docs.rs/clap/latest/clap/_cookbook/git_derive/index.html>
 
 #[derive(Parser)]
-struct Args {
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
+#[derive(Subcommand)]
+enum Commands {
+    Get {
+        path: String,
+    }
 }
 
 pub fn start() {
+    let args = Cli::parse();
 
+    match args.command {
+        Commands::Get { path } => {
+            println!("{path}");
+        }
+    }
 }
