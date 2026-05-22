1f7b82fe4eeab6ef3992bf0b591f52059ad6ee7c
Matthew Franchini <matchewf11@gmail.com>
2026-05-22 12:13:07 -0700

    foo

--- src/cli.rs
+++ src/cli.rs
@@ -6,6 +6,7 @@ use crate::hurler::{Error, Hurler};
 
 // <https://docs.rs/clap/latest/clap/_cookbook/git_derive/index.html>
 
+/// A better curl CLI
 #[derive(Parser)]
 #[command(about = "Hurl is a better curl!", long_about = None)]
 struct Cli {
@@ -15,8 +16,14 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Get a url
     Get {
+        /// Path to the url
         path: String,
+
+        /// Whether or not to pretty pring the output
+        #[arg(short, long)]
+        pretty: bool,
     }
 }
 
@@ -25,7 +32,9 @@ pub fn start() -> Result<(), Error> {
     let hurler = Hurler::new();
 
     match args.command {
-        Commands::Get { path } => {
+        Commands::Get { path, pretty } => {
+            let _ = pretty;
+
             hurler.get(path)
         }
     }
