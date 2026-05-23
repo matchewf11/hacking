f200dc27abee2cb0ae21990e67110be762665d11
Anthony <antnay@uw.edu>
2026-05-22 23:31:04 -0700

    remove mid nvim

--- editors/nvim/lua/wurl/init.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.setup(opts)
-  -- The plugin works purely through Neovim's builtin filetype and syntax features.
-  -- This setup function is provided for compatibility with standard Neovim plugin conventions,
-  -- and can be used in the future to handle configuration options if needed.
-end
-
-return M
